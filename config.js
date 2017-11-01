3b90a805fbd5474a18dd430fb3094116bb439d80
Oscar Blancarte <oscarblancarte3@gmail.com>
2017-10-31 18:07:51 -0600

    Se agregaron los datos de conexión de mongoDB al archivo config.js

--- config.js
+++ config.js
@@ -7,4 +7,12 @@ module.exports = {
   tweets: {
     maxTweetSize: 140
   },
+  mongodb: {
+    development: {
+      connectionString: "mongodb://twitter:J7qcSlyiwuOzYgiz@mini-twitter-shard-00-00-wwqzb.mongodb.net:27017,mini-twitter-shard-00-01-wwqzb.mongodb.net:27017,mini-twitter-shard-00-02-wwqzb.mongodb.net:27017/test?ssl=true&replicaSet=Mini-Twitter-shard-0&authSource=admin"
+    },
+    production: {
+      connectionString: "mongodb://twitter:1234@localhost:27017/Twitter?authSource=Twitter"
+    }
+  }
 }
