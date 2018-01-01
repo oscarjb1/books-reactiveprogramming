5ba851869da20f5ff5730d94644f65361842fc51
Oscar Blancarte <oscarblancarte3@gmail.com>
2018-01-01 15:15:33 -0600

    Se agregaron Tweet de prueba y se elimino la dependencia a los Modelos de Mongoose

--- InitMongoDB.js
+++ InitMongoDB.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 var configuration = require('./config')
-var Profile = require('./api/models/Profile')
+//var Profile = require('./api/models/Profile')
 var bcrypt = require('bcrypt')
 
 var opts = {
@@ -10,25 +10,48 @@ var opts = {
 }
 mongoose.connect(configuration.mongodb.development.connectionString, opts)
 
+
+var profile = new mongoose.Schema({}, { strict: false })
+var Profile = mongoose.model('profiles', profile)
+
+var tweet = new mongoose.Schema({}, { strict: false })
+var Tweet = mongoose.model('tweets', tweet)
+
 const newProfile = new Profile({
   name: "Usuario de prueba",
   userName: "test",
   password: bcrypt.hashSync('1234', 10)
 })
 
-Profile.findOne({username: 'test'}, function(err, queryUser){
+Profile.findOne({userName: 'test'}, function(err, queryUser){
   if(queryUser !== null){
     console.log("====> El usuario de prueba ya ha sido registrado")
     process.exit()
   }
 })
 
-newProfile.save(function(err){
-  if(err){
-    console.log("====> Error al crear el usuario de prueba",err)
-    process.exit()
-    return
+newProfile.save()
+.then(newUser => {
+
+  for( c = 1; c<=10; c++){
+    const newTweet = new Tweet({
+        _creator: newUser._id,
+        message: "Tweet de prueba " + c,
+    })
+
+    newTweet.save()
+    .then(savedTweet => {
+      console.log("Tweet creado correctamente");
+    }).catch(err => {
+      console.log("Error al guardar el Tweet " + err);
+    })
+
+    if(c == 9){
+      console.log("====> Usuario de prueba creado correctamente")
+    }
   }
-  console.log("====> Usuario de prueba creado correctamente")
+})
+.catch(err => {
+  console.log("====> Error al crear el usuario de prueba",err)
   process.exit()
 })
