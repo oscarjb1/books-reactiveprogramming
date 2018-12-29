636f84a92b93a1fd7edb9195e41758f779a41638
Oscar Blancarte <oscar_jb1@hotmail.com>
2018-12-28 21:41:54 -0600

    update library versiones

--- app/App.js
+++ app/App.js
@@ -5,10 +5,10 @@ class App extends React.Component{
 
   render(){
     return(
-      <React.Fragment>
+      <div>
         <h1>Hello World</h1>
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/210px-React-icon.svg.png"></img>
-      </React.Fragment>
+      </div>
     )
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'babel-loader',
       query:{
-        presets: ['es2015','react']
+        presets: ['env','react']
       }
     }]
   }
