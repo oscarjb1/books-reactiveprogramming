eed2afacb54244b4b2f5900ef2f938402974de44
oblancarte <oscar_jb1@hotmail.com>
2020-04-25 11:54:12 -0500

    Corrección de la ruta /signup

--- app/Signup.js
+++ app/Signup.js
@@ -2,6 +2,7 @@ import React from 'react'
 import update from 'immutability-helper'
 import APIInvoker from './utils/APIInvoker'
 import browserHistory from './History'
+import {Link} from 'react-router-dom'
 
 class Signup extends React.Component{
 
