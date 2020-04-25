64ab0302ac774deb82903043c731ae8222555e3e
oblancarte <oscar_jb1@hotmail.com>
2020-04-25 11:38:00 -0500

    Se modifica corrige la ruta /signup

--- app/AuthRoute.js
+++ app/AuthRoute.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
+import UserContext from './context/UserContext'
 
 class AuthRoute extends React.Component {
 
     render() {
-        const { component: Component, isLoged, ...rest } = this.props
+        const { component: Component, ...rest } = this.props
         return (<Route {...rest} render={(props) => (
-            isLoged
+            this.context
                 ? <Component {...props} />
                 : <Redirect to='/login' />
         )} />)
     }
 }
+AuthRoute.contextType = UserContext
 
 export default AuthRoute
