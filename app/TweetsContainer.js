529cacf41fabefb672cbb0f566ceea8d517e0415
oblancarte <oscar_jb1@hotmail.com>
2020-05-19 00:07:16 -0500

    Add InfinityScroll

--- app/TweetsContainer.js
+++ app/TweetsContainer.js
@@ -2,39 +2,51 @@ import React from 'react'
 import Tweet from './Tweet'
 import APIInvoker from "./utils/APIInvoker"
 import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroller'
 
-class TweetsContainer extends React.Component{
-  constructor(props){
+class TweetsContainer extends React.Component {
+  constructor(props) {
     super(props)
     this.state = {
+      hasMore: true,
       tweets: []
     }
-
-    let username = this.props.profile.userName
-    let onlyUserTweet = this.props.onlyUserTweet
-    this.loadTweets(username, onlyUserTweet)
+    this.loadMore = this.loadMore.bind(this)
   }
 
-  loadTweets(username, onlyUserTweet){
-    let url = '/tweets' + (onlyUserTweet  ? "/" + username : "")
+  loadTweets(username, onlyUserTweet, page) {
+    let currentPage = page || 0
+    const url = `/tweets${onlyUserTweet ? "/" + username : ""}?page=${currentPage}`
+
     APIInvoker.invokeGET(url, response => {
       this.setState({
-        tweets: response.body
+        tweets: this.state.tweets.concat(response.body),
+        hasMore: response.body.length >= 10
       })
-    },error => {
+    }, error => {
       console.log("Error al cargar los Tweets", error);
     })
   }
 
-  render(){
+  loadMore(page) {
+    const username = this.props.profile.userName
+    const onlyUserTweet = this.props.onlyUserTweet
+    this.loadTweets(username, onlyUserTweet, page - 1)
+  }
 
+  render() {
+    console.log("tweets => ", this.state.tweets)
     return (
       <main className="twitter-panel">
-        <If condition={this.state.tweets != null}>
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={this.loadMore}
+          hasMore={this.state.hasMore}
+          loader={<div className="loader" key={0}>Loading ...</div>} >
           <For each="tweet" of={this.state.tweets}>
-            <Tweet key={tweet._id} tweet={tweet}/>
+            <Tweet key={tweet._id} tweet={tweet} />
           </For>
-        </If>
+        </InfiniteScroll>
       </main>
     )
   }
